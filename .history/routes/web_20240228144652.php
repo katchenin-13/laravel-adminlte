386dfde6c2beb0a7147ca641d797fbae1b607f1a
katchenin-13 <katchenin03@gmail.com>
2024-02-28 14:49:57 +0530

    admin lte integre

--- .history/routes/web_20240228144652.php
+++ .history/routes/web_20240228144652.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
